b456dea3e4db6e5e62313a9ff481000dc0812f3b
baris12720 <baris12720@gmail.com>
2026-09-12 16:12:23 +0000

    Fix full screen resolution and orientation

--- app/src/main/java/com/okhedef/game/MainActivity.java
+++ app/src/main/java/com/okhedef/game/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_FULLSCREEN | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
