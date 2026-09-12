f4a46b6ae2cac93df7aa07859ea6e5b50217896e
baris12720 <baris12720@gmail.com>
2026-09-12 12:44:56 +0000

    OkHedefPro Release

---
+++ app/src/main/java/com/okhedef/game/TestBuild.java
@@ -0,0 +1,6 @@
+package com.okhedef.game;
+public class TestBuild {
+    public static void main(String[] args) {
+        System.out.println("OkHedefPro Java code validation successful!");
+    }
+}
