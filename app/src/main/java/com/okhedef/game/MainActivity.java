d866e1cfdf355958fa2bbded750acac44e3af2ca
baris12720 <baris12720@gmail.com>
2026-09-13 13:56:37 +0000

    Directly inject latest OkHedefPro-update sources

--- app/src/main/java/com/okhedef/game/MainActivity.java
+++ app/src/main/java/com/okhedef/game/MainActivity.java
@@ -1,36 +1,25 @@
 package com.okhedef.game;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.graphics.Color;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
-    private WebView web;
-    @Override public void onCreate(Bundle b) {
-        super.onCreate(b);
-        getWindow().setStatusBarColor(Color.rgb(7,11,22));
-        getWindow().setNavigationBarColor(Color.rgb(7,11,22));
-        web = new WebView(this);
-        web.setBackgroundColor(Color.rgb(7,11,22));
-        web.setWebViewClient(new WebViewClient());
-        web.setWebChromeClient(new WebChromeClient());
-        WebSettings s = web.getSettings();
-        s.setJavaScriptEnabled(true); s.setDomStorageEnabled(true); s.setDatabaseEnabled(true);
-        s.setAllowFileAccess(false); s.setAllowContentAccess(false);
-        s.setBuiltInZoomControls(false); s.setDisplayZoomControls(false);
-        s.setMediaPlaybackRequiresUserGesture(false);
-        web.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        setContentView(web);
-        web.loadUrl("file:///android_asset/index.html");
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
     }
-    @Override public void onBackPressed() {
-        if (web != null) web.evaluateJavascript("window.gamePause && gamePause()", null);
-        else super.onBackPressed();
-    }
-    @Override protected void onDestroy(){ if(web!=null) web.destroy(); super.onDestroy(); }
 }
